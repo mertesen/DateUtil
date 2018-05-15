2a53fa430d52cf3cfbfafbabf535f8811e791998
Mert Esen <mertesen3@gmail.com>
2018-05-16 01:59:29 +0300

    Added "than" methods

--- src/com/mertapp/util/Constants.java
+++ src/com/mertapp/util/Constants.java
@@ -1,5 +1,7 @@
 package com.mertapp.util;
 
 public class Constants {
-
+	public static final Integer INT_ZERO = 0;
+	public static final String longDateFormat = "dd/MM/yyyy hh:mm:ss";
+	public static final String shortDateFormat = "dd/MM/yyyy";
 }
--- src/com/mertapp/util/DateUtils.java
+++ src/com/mertapp/util/DateUtils.java
@@ -15,4 +15,22 @@ public class DateUtils {
 		calendar.add(Calendar.DATE, dayCount);
 		return calendar.getTime();
 	}
+	
+	public static Calendar toCalendar(Date inputDate) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(inputDate);
+		return calendar;
+	}
+	
+	public static boolean smallerThan(Date inputDate_1, Date inputDate_2) {
+		return inputDate_1.compareTo(inputDate_2) < Constants.INT_ZERO;
+	}
+	
+	public static boolean biggerThan(Date inputDate_1, Date inputDate_2) {
+		return inputDate_1.compareTo(inputDate_2) > Constants.INT_ZERO;
+	}
+	
+	public static boolean equalTo(Date inputDate_1, Date inputDate_2) {
+		return inputDate_1.compareTo(inputDate_2) == Constants.INT_ZERO;
+	}
 }
