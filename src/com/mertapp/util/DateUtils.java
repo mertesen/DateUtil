19ede92e9d2f409e38ecb972bd50ec8dce4f70bb
Mert <mertesen3@gmail.com>
2018-05-18 18:00:25 +0300

    between and toString methods added

--- src/com/mertapp/util/DateUtils.java
+++ src/com/mertapp/util/DateUtils.java
@@ -1,7 +1,9 @@
 package com.mertapp.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import com.mertapp.util.Constants;;
 
 public class DateUtils {
 	public static Date getCurrentDate() {
@@ -32,5 +34,14 @@ public class DateUtils {
 	
 	public static boolean equalTo(Date inputDate_1, Date inputDate_2) {
 		return inputDate_1.compareTo(inputDate_2) == Constants.INT_ZERO;
+	}
+	
+	public static String DateToString(Date inputDate, String dateFormat) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+		return simpleDateFormat.format(inputDate);
+	}
+	
+	public static Boolean between(Date inputDate, Date minDate, Date maxDate, Boolean isLimitDateIncluded) {
+		return null;
 	}
 }
